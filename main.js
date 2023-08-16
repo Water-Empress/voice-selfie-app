c3f109737cac9a03c6d9fd8bec2908942fc4fb36
Water-Empress <110268568+Water-Empress@users.noreply.github.com>
2023-08-15 17:58:06 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -23,13 +23,19 @@ recognition.onresult = function(event) {
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").innerHTML;
+    speak_data = " Taking your selfie in five seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
 
     Webcam.attach(camera);
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    }, 5000);
+    
 }
 
  
@@ -40,3 +46,19 @@ Webcam.set({
     image_format : 'jpeg',
     jpeg_quality:90
 });
+ camera= document.getElementById("camera");
+
+ function take_snapshot()
+ {
+    Webcam.snap (function(data_uri){
+        document.getElementById("result").innerHTML = '<img id ="selfie_image" src= "'+data_uri+'"/>';
+    });
+ }
+ 
+ function save()
+ {
+    link =document.getElementById("link");
+    image= document.getElementById("selfie_image").src ;
+    link.href=image;
+    link.click
+ }
